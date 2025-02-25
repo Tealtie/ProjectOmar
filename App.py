1e2b3160246caf9510d5c3ea7c0c48b4e2fb94d9
Tealtie <125929994+Tealtie@users.noreply.github.com>
2025-02-25 20:30:34 +0000

    Update game title and add welcome message to the screen

--- App.py
+++ App.py
@@ -3,9 +3,9 @@ import random
 
 WIDTH = 800
 HEIGHT = 600
-TITLE = "PROJECT OMAR"
+TITLE = "Project Omar"
 
 def draw():
     screen.fill((0, 0, 0))
-
+    screen.draw.text("Project Omar is Goated!", (300, 300), color="white")
 pgzrun.go()
